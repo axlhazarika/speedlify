0751ecb157d55408c115295a2b5e05776c53d2bb
axlhazarika <axl@axlhazarika.in>
2021-01-22 21:48:57 +0530

    Update sample.js

--- _data/sites/sample.js
+++ _data/sites/sample.js
@@ -5,6 +5,6 @@ module.exports = {
 		frequency: 60 * 23, // (in minutes), 23 hours
 	},
 	urls: [
-		"https://delightful-pebble-06bd83010.azurestaticapps.net/"
+		"https://axlhazarika.vercel.app/"
 	]
 };
